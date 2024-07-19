47e47bcfff1b39d13a003d81b4c3aae291942c1c
Seven <76436489+Zetonen@users.noreply.github.com>
2024-07-18 23:28:24 -0700

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { storeAppState } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/rout/">
-      <App />
-    </BrowserRouter>
+    <Provider store={storeAppState}>
+      <BrowserRouter basename="/rout/">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
